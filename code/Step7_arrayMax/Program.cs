65e29431b0e33f563da3710c06c9ae17c8982f1a
AnastasiaZlamanyuk <nastya.zlamanyuk@bk.ru>
2026-03-07 22:10:25 +0300

    Step 7 arrays

---
+++ code/Step7_arrayMax/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] array = [1, 3, 5, 7, 9];
+Console.WriteLine("Поиск максимального элемента массива");
+int max = array[0];
+for (int i = 1; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+}
+Console.WriteLine($"Максимальный элемент: {max}");
