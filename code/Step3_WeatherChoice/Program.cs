c51b53bde3c0718fdacc142365da23bdbbe46c4b
AnastasiaZlamanyuk <nastya.zlamanyuk@bk.ru>
2026-03-07 21:23:54 +0300

    Step 4: variables, data types and calculations

--- code/Step3_WeatherChoice/Program.cs
+++ code/Step3_WeatherChoice/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Выбор одежды по погоде");
+Console.Write("Введите текущую температуру (℃): ");
+int temperature = Convert.ToInt32(Console.ReadLine());
+if (temperature >= 20)
+{
+    Console.WriteLine("Наденьте легкую одежду");
+}
+else
+{
+    Console.WriteLine("Наденьте теплую одежду");
+}
+Console.WriteLine("Хорошего дня!");
