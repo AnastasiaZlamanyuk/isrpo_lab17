de03776cf8a768a31f7a33919c289a9cbf1ba24e
AnastasiaZlamanyuk <nastya.zlamanyuk@bk.ru>
2026-03-08 13:16:48 +0300

    Completed Lab: Algorithms and Flowcharts

---
+++ examples/Task1_BMI/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Калькулятор ИМТ");
+Console.Write("Введите ваш вес (в кг): ");
+double weight = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите ваш рост (в метрах): ");
+double height = Convert.ToDouble(Console.ReadLine());
+double bmi = weight / (height * height);
+bmi = Math.Round(bmi, 1);
+Console.WriteLine($"\nВаш ИМТ: {bmi}");
+string category = "";
+string recommendation = "";
+if (bmi < 18.5)
+{
+category = "Недостаточный вес";
+recommendation = "Увеличить калорийность питания";
+}
+else if (bmi >= 18.5 && bmi <= 24.9)
+{
+category = "Нормальный вес";
+recommendation = "Поддерживать текущий образ жизни";
+}
+else if (bmi >= 25 && bmi <= 29.9)
+{
+category = "Избыточный вес";
+recommendation = "Увеличить физическую активность";
+}
+else
+{
+category = "Ожирение";
+recommendation = "Обратиться к врачу-диетологу";
+}
+Console.WriteLine($"Категория: {category}");
+Console.WriteLine($"Рекомендация: {recommendation}");
---
+++ examples/Task3_GuessGame/Program.cs
@@ -0,0 +1,46 @@
+﻿Random rnd = new Random();
+while (true)
+{
+    int rndNum = rnd.Next(1, 101);
+    int attempts = 0;
+    Console.WriteLine("Загадано случайное число от 1 до 100");
+    Console.WriteLine("(Для выхода введите 0)\n");
+    while (true)
+    {
+        Console.Write("Введите число: ");
+        if (!int.TryParse(Console.ReadLine(), out int userNum))
+        {
+            Console.WriteLine("Ошибка! Введите целое число.\n");
+            continue;
+        }
+        if (userNum == 0)
+        {
+            Console.WriteLine($"\nВыход из игры. Загаданное число было: {rndNum}");
+            return;
+        }
+        if (userNum < 1 || userNum > 100)
+        {
+            Console.WriteLine("Число должно быть от 1 до 100!\n");
+            continue;
+        }
+        attempts++;
+        if (userNum == rndNum)
+        {
+            Console.WriteLine($"\nВы угадали! Число {rndNum}!");
+            Console.WriteLine($"Количество попыток: {attempts}");
+            break;
+        }
+        if (userNum > rndNum)
+            Console.WriteLine($"Ваше число больше (попытка №{attempts})");
+        else
+            Console.WriteLine($"Ваше число меньше (попытка №{attempts})");
+        Console.WriteLine();
+    }
+    Console.WriteLine("\nХотите сыграть ещё? (да/нет)");
+    string answer = Console.ReadLine().ToLower();
+    if (answer == "нет" && answer == "no")
+    {
+        Console.WriteLine("Спасибо за игру! До свидания!");
+        break;
+    }
+}
